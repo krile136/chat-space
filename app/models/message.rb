c9f5e016b888cc9c29ebdf832b701da5c523ecd6
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-07-17 01:01:44 +0900

    generated model, routing, controller

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
-  belongs_to :user
   belongs_to :group
+  belongs_to :user
+
+  validates :content, presence: true, unless: :image?
+  # imageがないときに、contentが存在していないと保存できない
+  # 逆に、imageがあるときはcontentの有無によらず保存される
 end
