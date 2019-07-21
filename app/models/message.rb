f091eecf0f06f11fbb6a2a2a72e5af78c904a24d
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-07-21 12:43:20 +0900

    generated message sending function

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,6 @@ class Message < ApplicationRecord
   validates :content, presence: true, unless: :image?
   # imageがないときに、contentが存在していないと保存できない
   # 逆に、imageがあるときはcontentの有無によらず保存される
+
+  mount_uploader :image, ImageUploader
 end
